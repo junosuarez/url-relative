d8d91f9810f9e3b2c520ef0dabf5e7f7e881b37e
Dan Allen <dan.j.allen@gmail.com>
2017-12-14 19:56:32 -0700

    resolves #1 fix calculation between directory index paths
    - add extra segment to exit directory when from is a directory index path
    - add tests to cover scenarios when from is a directory index path

--- test/index.js
+++ test/index.js
@@ -3,31 +3,43 @@ var test = require('tap').test;
 var relative = require('../index');
 
 test('different domain', function (t) {
-  t.plan(1);
+  t.plan(2);
   t.equal(
     relative('http://a.com:12/a', 'http://b.com/a'),
     'http://b.com/a');
+  t.equal(
+    relative('http://a.com:12/a/', 'http://b.com/a/'),
+    'http://b.com/a/');
 });
 
 test('same domain', function (t) {
-  t.plan(1);
+  t.plan(2);
   t.equal(
     relative('http://a.com/a', 'http://a.com/b'),
     'b');
+  t.equal(
+    relative('http://a.com/a/', 'http://a.com/b/'),
+    '../b/');
 });
 
 test('divergent paths, longer from', function (t) {
-  t.plan(2);
+  t.plan(4);
   t.equal(
     relative('/a/b/c/d','/a/b/d'),
     '../d');
   t.equal(
     relative('/a/b/c/d/e','/a/d/e'),
     '../../d/e');
+  t.equal(
+    relative('/a/b/c/d/','/a/b/d/'),
+    '../../d/');
+  t.equal(
+    relative('/a/b/c/d/e/','/a/d/e/'),
+    '../../../d/e/');
 });
 
 test('divergent paths, longer to', function (t) {
-  t.plan(3);
+  t.plan(6);
   t.equal(
     relative('/a/b/c/d','/a/b/c/d/e'),
     'e');
@@ -37,11 +49,23 @@ test('divergent paths, longer to', function (t) {
   t.equal(
     relative('/','/a/b'),
     'a/b');
+  t.equal(
+    relative('/a/b/c/d/','/a/b/c/d/e/'),
+    'e/');
+  t.equal(
+    relative('/a/b/c/d/','/a/b/c/d/e/f/'),
+    'e/f/');
+  t.equal(
+    relative('/','/a/b/'),
+    'a/b/');
 });
 
 test('divergent paths, equal length', function (t) {
-  t.plan(1);
+  t.plan(2);
   t.equal(
     relative('/a/b/c/d/e/f','/a/b/c/g/h/j'),
     '../../g/h/j');
+  t.equal(
+    relative('/a/b/c/d/e/f/','/a/b/c/g/h/j/'),
+    '../../../g/h/j/');
 });
